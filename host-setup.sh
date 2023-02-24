51709964598b7a777cce0ed1ed9e153fb74c9eb6
allen O <aoster@mirantis.com>
2023-02-24 16:30:18 -0500

    Updating install scripts

---
+++ host-setup.sh
@@ -0,0 +1,91 @@
+# The goal of this script is to install the required tools for isntall k8s using kubeadm. 
+#install base tools and setup kube repo
+sudo apt install curl apt-transport-https -y
+curl -fsSL  https://packages.cloud.google.com/apt/doc/apt-key.gpg|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/k8s.gpg
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# install kubectl, kubeadm and kubelet
+sudo apt update
+sudo apt install wget curl vim git kubelet kubeadm kubectl -y
+sudo apt-mark hold kubelet kubeadm kubectl
+
+#disable swap
+sudo swapoff -a 
+
+#set kernal features and settings
+sudo modprobe overlay
+sudo modprobe br_netfilter
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+sudo sysctl --system
+
+# Install docker
+# Add repo and Install packages
+sudo apt update
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y containerd.io docker-ce docker-ce-cli
+
+# Create required directories
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+# Create daemon json config file
+sudo tee /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+# Start and enable Services
+sudo systemctl daemon-reload 
+sudo systemctl restart docker
+sudo systemctl enable docker
+
+# Configure persistent loading of modules
+sudo tee /etc/modules-load.d/k8s.conf <<EOF
+overlay
+br_netfilter
+EOF
+
+# Ensure you load modules
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Set up required sysctl params
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+# Install cri-dockerd as a docker shim for kubernetes
+git clone https://github.com/Mirantis/cri-dockerd.git
+
+###Install GO###
+wget https://storage.googleapis.com/golang/getgo/installer_linux
+chmod +x ./installer_linux
+./installer_linux
+source ~/.bash_profile
+
+pushd cri-dockerd
+mkdir bin
+go build -o bin/cri-dockerd
+mkdir -p /usr/local/bin
+install -o root -g root -m 0755 bin/cri-dockerd /usr/local/bin/cri-dockerd
+cp -a packaging/systemd/* /etc/systemd/system
+sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+systemctl daemon-reload
+systemctl enable cri-docker.service
+systemctl enable --now cri-docker.socket
+popd
---
+++ k8s-init.sh
@@ -0,0 +1,21 @@
+# Initialize the cluster! 
+sudo systemctl enable kubelet
+# set token for easy reuse later:
+token=$(kubeadm token generate)
+
+#init kubernetes
+sudo kubeadm init --token $token \
+    --kubernetes-version 1.26.1 \
+    --upload-certs \
+    --apiserver-advertise-address $PrivateIP \
+    --pod-network-cidr 192.168.0.0/16 \
+    --cri-socket /run/cri-dockerd.sock 
+
+# export config
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+# Get calico operator and custom resources
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/tigera-operator.yaml -o calico-operator.yml
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/custom-resources.yaml -O
+kubectl create -f caalico-operator.yml
+kubectl create -f custom-resources.yaml
