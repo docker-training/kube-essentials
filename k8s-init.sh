cc3f6c47be4292a544f59c92005efd9325f84085
aoster-mira <78741753+aoster-mira@users.noreply.github.com>
2023-02-28 08:25:36 -0500

    fix typo in calico operator

--- k8s-init.sh
+++ k8s-init.sh
@@ -17,5 +17,5 @@ export KUBECONFIG=/etc/kubernetes/admin.conf
 # Get calico operator and custom resources
 curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/tigera-operator.yaml -o calico-operator.yml
 curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/custom-resources.yaml -O
-kubectl create -f caalico-operator.yml
+kubectl create -f calico-operator.yml
 kubectl create -f custom-resources.yaml
